c5a7cd95f735eada125949a9f8bc7caeff5d9ce5
Nick Leso <nickleso.work@gmail.com>
2022-09-03 13:48:53 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+};
 
+const COLOR_SWITCHER_DELAY = 1000;
+let intervalId = 1;
+refs.buttonStop.disabled = true;
+
+refs.buttonStart.addEventListener('click', onButtonClickStartSwitcher);
+refs.buttonStop.addEventListener('click', onButtonClickStopSwitcher);
+
+function onButtonClickStartSwitcher() {
+  intervalId = setInterval(colorSwitcher, COLOR_SWITCHER_DELAY);
+  refs.buttonStart.disabled = true;
+  refs.buttonStop.disabled = false;
+
+  console.log('start');
+}
+
+function onButtonClickStopSwitcher() {
+  clearInterval(intervalId);
+  refs.buttonStart.disabled = false;
+  refs.buttonStop.disabled = true;
+  console.log('Stop');
+}
+
+function colorSwitcher() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  console.log(`Current color HEX ${randomColor}`);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
